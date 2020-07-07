784591f9957902ddc80b31af012c5d48e2b1f5a9
samuel <samu714@stanford.com>
2020-07-07 17:55:46 -0500

    primer commit

---
+++ probar.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from sumar import sumar
+
+print("hello world")
+
+datos={"nombres":["samuel","brenda","vivi"],"edades":[24,24,24]}
+df=pd.DataFrame(datos)
+print(df)
+
+print(sumar(3,4),"sumando 3 +4")
+print("hello python")
+
+dataframe_csv2=pd.read_csv("C:/Users/Samuel Cueva/Desktop/actividad 1.csv")
