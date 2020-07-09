92cbf51fda6846b8af43726182c1002e31dfaa60
samuel <samu714@stanford.com>
2020-07-09 16:14:41 -0500

    agregamos la funión dividir

--- probar.py
+++ probar.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sumar import sumar,restar
+from sumar import sumar,restar,dividir
 
 print(" hello Pandas")
 
@@ -12,5 +12,7 @@ print(sumar_triplicar(3,4),"sumando 3 +4")
 print("hello python")
 
 print(restar(8,5))
+
+print(dividir(10,2))
 dataframe_csv2=pd.read_csv("C:/Users/Samuel Cueva/Desktop/actividad 1.csv")
 print(dataframe_csv2)
--- sumar.py
+++ sumar.py
@@ -4,3 +4,9 @@ def sumar_triplicar(n1,n2):
 
 def restar(n1,n2):
     return n1-n2
+
+
+def dividir(n1,n2):
+    if n2!=0:
+
+        return n1/n2
