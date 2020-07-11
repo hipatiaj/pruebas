fc3738548d00ca99d62e47137eff3f0993a1012b
samuel <samu714@stanford.com>
2020-07-11 17:30:43 -0500

    se aceptó nuevos cambios en sumar.py

--- sumar.py
+++ sumar.py
@@ -7,16 +7,8 @@ def sumar_triplicar(n1,n2):
 def restar(n1,n2):
     return n1-n2
 
-<<<<<<< HEAD
-
-def dividir(n1,n2):
-    if n2!=0:
-
-        return n1/n2
-=======
 def cuadrado(n):
     return n*n
 
 def raiz_cuadrada(n):
     return np.sqrt(n)
->>>>>>> test3
