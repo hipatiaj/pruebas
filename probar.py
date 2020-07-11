2e32ad4b3d84a85c6fd3779a17a984abcddd78f6
samuel <samu714@stanford.com>
2020-07-11 17:10:47 -0500

    aceptar cambios nuevos

--- probar.py
+++ probar.py
@@ -1,10 +1,6 @@
 import numpy as np
 import pandas as pd
-<<<<<<< HEAD
 from sumar import sumar,restar,dividir,cuadrado,raiz_cuadrada
-=======
-from sumar import sumar,restar,dividir,cuadrado,raiz_cuadrada
->>>>>>> test3
 
 print(" hello Pandas")
 
@@ -16,14 +12,8 @@ print(sumar_triplicar(3,4),"sumando 3 +4")
 print("hello python")
 
 print(restar(8,5))
-<<<<<<< HEAD
 print(dividir(10,2))
 print(cuadrado(4))
 print(raiz_cuadrada(16))
-=======
-print(dividir(10,2))
-print(cuadrado(4))
-print(raiz_cuadrada(16))
->>>>>>> test3
 dataframe_csv2=pd.read_csv("C:/Users/Samuel Cueva/Desktop/actividad 1.csv")
 print(dataframe_csv2)
